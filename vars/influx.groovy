7d01d707e9dd2c449117f3456fad8a57171706b7
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-11 19:54:39 +0530

    Update influx.groovy

--- vars/influx.groovy
+++ vars/influx.groovy
@@ -6,9 +6,9 @@ def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
 def total = resultJson
-  def commit[0]=total.commits
-  echo "$commit[0]"
-sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'gitlabcommit =${commit[0]}' 
+  def commit[]=total.commits
+  echo "$commit[]"
+sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'gitlabcommit =${commit[]}' 
 """
 }
 
