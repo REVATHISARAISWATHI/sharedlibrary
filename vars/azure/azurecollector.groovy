32c1df2b230c079cd093a85bc8b426ad37b3864d
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-11 19:47:44 +0530

    Create azurecollector.groovy

---
+++ vars/azure/azurecollector.groovy
@@ -0,0 +1,5 @@
+def call()
+{
+def build= sh 'curl -s --user vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya
+https://dev.azure.com/vickysastryvs/d2/_apis/wit/workitems?ids=1,4,3&api-version=5.1 -o ouput.json'
+}
