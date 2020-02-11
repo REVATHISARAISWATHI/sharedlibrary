a1afe97ab30ba5f0052779d4785c333f7c6f9590
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-11 21:02:51 +0530

    Create azureblocklevel.groovy

---
+++ vars/azureblocklevel.groovy
@@ -0,0 +1,4 @@
+def call()
+{
+def build= sh 'curl -s  https://dev.azure.com/vickysastryvs/d2/_apis/work/boardcolumns --user vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya  -o ouput.json'
+}
