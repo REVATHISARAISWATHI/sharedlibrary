f436f36489c851ac82f7146d9b2cf4681ea78203
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-11 19:18:54 +0530

    Update commits.groovy

--- vars/commits.groovy
+++ vars/commits.groovy
@@ -1,5 +1,5 @@
 def call(){
 //sh "curl -X GET -i -H  -d https://gitlab.com/api/v4/projects/16837979/repository/commits"
 def jso=sh "curl -s https://gitlab.com/api/v4/projects/16837979/repository/commits --user Priyakumar:przqBFp-r6X7K_3okz5v"
-  println(jso)
+  echo "$jso"
 }
