79923aaf75e8dbe22488535cdbc5c40a8f98b0c6
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-11 20:08:50 +0530

    Update influx.groovy

--- vars/influx.groovy
+++ vars/influx.groovy
@@ -7,7 +7,7 @@ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/
 def resultJson = jsonSlurper.parse(reader)
 def total = resultJson
   def commit=total.commits
-commit.replace(/[\[\]]+/g,'')
+String oldpass=commit.replace(/[\[\]]+/g,'')
   echo "$commit"
 sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'gitlabcommit =${oldpass}' 
 """
