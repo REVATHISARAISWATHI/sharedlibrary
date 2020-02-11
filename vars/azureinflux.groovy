88b6e9bf5840d199f41a1423544911a13ad320af
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-11 20:08:26 +0530

    Update azureinflux.groovy

--- vars/azureinflux.groovy
+++ vars/azureinflux.groovy
@@ -1,3 +1,4 @@
+
 import groovy.json.*
 
 @NonCPS
@@ -5,15 +6,8 @@ create(){
 def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
-def no= resultJson.count
+def count = resultJson.count
  
-  
-sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'azure no.of work items =${no}' 
+sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'jira issues=${count}' 
 """
-}
-
-
-def call()
-{
-create()
 }
