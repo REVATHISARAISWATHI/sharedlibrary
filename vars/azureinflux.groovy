06728f24b866382d7d5e86e31908a08a7fd8f12c
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-11 20:09:52 +0530

    Update azureinflux.groovy

--- vars/azureinflux.groovy
+++ vars/azureinflux.groovy
@@ -11,3 +11,7 @@ def count = resultJson.count
 sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'jira issues=${count}' 
 """
 }
+def call()
+{
+ create()
+}
