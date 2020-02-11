b47b782960f4463daf347f882c206f7f8eeb1448
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-11 21:06:48 +0530

    Update lastfailurebuild.groovy

--- vars/lastfailurebuild.groovy
+++ vars/lastfailurebuild.groovy
@@ -1,3 +1,6 @@
-def call(job_name){
-  sh "curl -s http://18.188.152.185:8080/job/'${job_name}'/lastFailedBuild/api/json --user vj:11e428e94b267ffbab27fa713e2da8e6e8 -o output.json"
+def call(JSON){
+  String a=jsonObj.ci.jobs.job.job_name
+String JobName=a.replaceAll("\\[", "").replaceAll("\\]","");
+  
+  sh "curl -s http://18.188.152.185:8080/job/'${JobName}'/lastFailedBuild/api/json --user vj:11e428e94b267ffbab27fa713e2da8e6e8 -o output.json"
 }
