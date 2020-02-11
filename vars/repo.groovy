0cdd4103c8fea23b12c5a699b2419321f93d5850
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-11 20:07:44 +0530

    Update repo.groovy

--- vars/repo.groovy
+++ vars/repo.groovy
@@ -8,5 +8,5 @@ String repoid=b.replaceAll("\\[", "").replaceAll("\\]","");
    // withCredentials([usernamePassword(credentialsId: 'nexus_cred', passwordVariable: 'password', usernameVariable:'username')]) {
        sh'  curl -X GET -i -H "Accept:application/json" -d  -u admin:admin123 "http://3.15.18.214:8081/nexus/service/local/repositories/${repoid}/status" '
  
-}
+
 }
