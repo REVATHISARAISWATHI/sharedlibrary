0a1b495378de0c249ade0f15544089732bd36eea
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-11 19:16:18 +0530

    Update lastsuccessfulbuild.groovy

--- vars/lastsuccessfulbuild.groovy
+++ vars/lastsuccessfulbuild.groovy
@@ -1,4 +1,4 @@
 def call(){
 def json= sh "curl -s http://18.188.152.185:8080/job/bamboo1/lastSuccessfulBuild/api/json --user vj:11e428e94b267ffbab27fa713e2da8e6e8"
-  echo "${json}"
+  println(json)
 }
