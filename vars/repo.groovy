0f8ee4de5d2aa7a27eb2abdffcb8a1f892f7bcb6
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-11 20:36:12 +0530

    Update repo.groovy

--- vars/repo.groovy
+++ vars/repo.groovy
@@ -2,7 +2,7 @@ def call(){
 
 
    // withCredentials([usernamePassword(credentialsId: 'nexus_cred', passwordVariable: 'password', usernameVariable:'username')]) {
-      def status= sh'  curl -X GET -i -H "Accept:application/json" -d  -u admin:admin123 "http://3.15.18.214:8081/nexus/service/local/repositories/id1/status" -o output1.json '
+      def status= sh 'curl -X GET -i -H  "Accept:application/json" -d  -u admin:admin123 "http://3.15.18.214:8081/nexus/service/local/repositories/id1/status" -o output1.json '
  
 
 }
