2f87b71759cc2b93bb52424f06465dbe6caa1c65
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-12 12:35:11 +0530

    Update moveinflux.groovy

--- vars/moveinflux.groovy
+++ vars/moveinflux.groovy
@@ -5,7 +5,7 @@ create(){
 def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
-def bno = resultJson.number
+def bno = resultJson.id
   echo "$bno"
 sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'jenkins lastfailurebuildno=${bno}' 
 """
