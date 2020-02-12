e1091115e7a2107900a48c993e4cd4a0ed215336
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-12 12:22:32 +0530

    Update lastfailurebuild.groovy

--- vars/lastfailurebuild.groovy
+++ vars/lastfailurebuild.groovy
@@ -1,4 +1,8 @@
-def call(jobname){
+def call(JSON){
+  def jsonString = JSON
+def jsonObj = readJSON text: jsonString
+println(jsonObj.ci1)
+  
 def build=sh "curl -s http://18.188.152.185:8080/job/'${jobname}'/lastFailedBuild/api/json --user vj:11e428e94b267ffbab27fa713e2da8e6e8"
 println(build)
 }
