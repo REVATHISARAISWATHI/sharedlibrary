69f8dd53e6ce8ea5662f5d20b422d7cd466223e4
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-12 16:56:57 +0530

    Update Ndown_artifacts.groovy

--- vars/Ndown_artifacts.groovy
+++ vars/Ndown_artifacts.groovy
@@ -1,19 +1,19 @@
 def call(){
-//def jsonString = JSON
+def jsonString = JSON
 //println(jsonString)
-//def jsonObj = readJSON text: jsonString
+def jsonObj = readJSON text: jsonString
 //println(jsonObj.brm)
 
 //String a=jsonObj.brm.password.fileName
 //String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
-//String b=jsonObj.brm.password.fileName
-//String fn=b.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.brm.password.fileName
+String fn=b.replaceAll("\\[", "").replaceAll("\\]","");
 
 
 //httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
         //withCredentials([usernamePassword(credentialsId: 'nexus_cred', passwordVariable: 'password', usernameVariable:'username')]) {
 
-  sh 'wget  --user=admin --password=admin123 "http://3.15.18.214:8081/nexus/service/local/repositories/pri/content/dynamic_new-1.0.war" '
+  sh 'wget  --user=admin --password=admin123 "http://3.15.18.214:8081/nexus/service/local/repositories/pri/content/Codekillers/dynamic_new/1.0/${fn}" '
     // httpMode: 'GET', wget --user=admin --password=admin123 "http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/dynamic_new-1.0.war"
     
    // httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/dynamic_new-1.0.war" 
