1d50f1ec2f38651f6e81fa0858f35ad6e236ae01
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-12 12:57:52 +0530

    Update Nartifact_download.groovy

--- vars/Nartifact_download.groovy
+++ vars/Nartifact_download.groovy
@@ -1,15 +1,15 @@
-def call(){
-//def jsonString = JSON
+def call(JSON){
+def jsonString = JSON
 //println(jsonString)
-//def jsonObj = readJSON text: jsonString
+def jsonObj = readJSON text: jsonString
 //println(jsonObj.brm)
 
-//String a=jsonObj.data.name
-//String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+String a=jsonObj.brm.password.fileName
+String fn=a.replaceAll("\\[", "").replaceAll("\\]","");
 //String b=jsonObj.brm.repositories.repository.id
 //String repoid=b.replaceAll("\\[", "").replaceAll("\\]","");
     withCredentials([usernamePassword(credentialsId: 'nexus_cred', passwordVariable: 'password', usernameVariable:'username')]) {
 
-        sh ' curl -v -F r=pri -F hasPom=true -F e=war -F file=@pom.xml -F file=@**/*.war -u admin:admin123 "http://3.15.18.214:8081/nexus/service/local/artifact/maven/content" '
+        sh ' curl -v -F r=pri -F hasPom=true -F e=war -F file=@pom.xml -F file=${fn} -u admin:admin123 "http://3.15.18.214:8081/nexus/service/local/artifact/maven/content" '
 }
 }
