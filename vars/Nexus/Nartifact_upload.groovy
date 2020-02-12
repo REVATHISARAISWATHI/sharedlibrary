65da44e78e07c152acb522fdd24ad5e26b018c13
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-12 15:19:29 +0530

    Create Nartifact_upload.groovy

---
+++ vars/Nexus/Nartifact_upload.groovy
@@ -0,0 +1,14 @@
+def call(JSON){
+def jsonString = JSON
+def jsonObj = readJSON text: jsonString
+println(jsonObj.brm)
+
+String a=jsonObj.brm.password.fileName
+String fn=a.replaceAll("\\[", "").replaceAll("\\]","");
+//String b=jsonObj.brm.repositories.repository.id
+//String repoid=b.replaceAll("\\[", "").replaceAll("\\]","");
+    withCredentials([usernamePassword(credentialsId: 'nexus_cred', passwordVariable: 'password', usernameVariable:'username')]) {
+
+        sh  "curl -v -F r=pri -F hasPom=true -F e=war -F file=@pom.xml -F file=@target/'${fn}' -u $username:$password http://3.15.18.214:8081/nexus/service/local/artifact/maven/content" 
+}
+}
