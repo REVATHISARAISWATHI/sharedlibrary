c68eae7ea3827f46a8efda13fe564e51524fb34e
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-12 12:19:33 +0530

    Create Azurebackloglevel.groovy

---
+++ vars/Azurebackloglevel.groovy
@@ -0,0 +1,23 @@
+import groovy.json.*
+
+@NonCPS
+influx(){
+def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+def count = resultJson.count
+ 
+sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'azureboard backloglevel=${count}' 
+"""
+}
+def call()
+{
+ try{
+sh 'curl -s GET https://dev.azure.com/vickysastryvs/d2/d2 Team/_apis/work/backlogs?api-version=5.1-preview.1 --user vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya  -o ouput.json'
+influx()
+ }catch(Exception e)
+ {
+  println("Exception : url error")
+ }
+  
+}
