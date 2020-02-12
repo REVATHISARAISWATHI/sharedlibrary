c986ff75a11614cfdffb5c98492ad9e905684f2a
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-12 17:05:10 +0530

    Update art.groovy

--- vars/art.groovy
+++ vars/art.groovy
@@ -1,3 +1,5 @@
 def call(){
+  withCredentials([usernamePassword(credentialsId: 'nexus_cred', passwordVariable: 'password', usernameVariable:'username')]) {
   sh 'wget  --user=admin --password=admin123 "http://3.15.18.214:8081/nexus/service/local/repositories/pri/content/Codekillers/dynamic_new/1.0/dynamic_new-1.0.war" '
+}
 }
