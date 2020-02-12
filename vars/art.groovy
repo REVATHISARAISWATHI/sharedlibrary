23a8d0fca80b1366d5a2f679ab5d8654ac9bbe4a
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-12 16:50:30 +0530

    Update art.groovy

--- vars/art.groovy
+++ vars/art.groovy
@@ -1,3 +1,3 @@
 def call(){
-  sh 'wget  --user=admin --password=admin123 "http://3.15.18.214:8081/nexus/service/local/repositories/pri/content/dynamic_new-1.0.war" '
+  sh 'wget  --user=admin --password=admin123 "http://3.15.18.214:8081/nexus/service/local/repositories/pri/content/Codekillers/dynamic_new/1.0/dynamic_new-1.0.war" '
 }
