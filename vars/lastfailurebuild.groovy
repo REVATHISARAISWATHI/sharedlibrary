699f1703deafadd66e00c732c30d3e78e3db9058
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-12 12:24:52 +0530

    Update lastfailurebuild.groovy

--- vars/lastfailurebuild.groovy
+++ vars/lastfailurebuild.groovy
@@ -4,6 +4,7 @@ def jsonObj = readJSON text: jsonString
 println(jsonObj.ci1)
 String a=jsonObj.ci1.jobs.job.job_name
 String jobname=a.replaceAll("\\[", "").replaceAll("\\]","");
+println(jobname)
   
 def build=sh "curl -s http://18.188.152.185:8080/job/'${jobname}'/lastFailedBuild/api/json --user vj:11e428e94b267ffbab27fa713e2da8e6e8"
 println(build)
