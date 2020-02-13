645001508b02e35424ac9f6f929964a719dfe9e5
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-13 16:31:47 +0530

    Update Aboardcolumn.groovy

--- vars/Aboardcolumn.groovy
+++ vars/Aboardcolumn.groovy
@@ -12,12 +12,17 @@ sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:808
 }
 def call()
 {
- try{
-sh 'curl -s GET https://dev.azure.com/vickysastryvs/d2/_apis/work/boardcolumns?api-version=5.1 --user vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya  -o ouput.json'
-influx()
- }catch(Exception e)
- {
-  println("Exception : url error")
- }
-  
+ 
+sh 'curl -s GET https://dev.azure.com/vickysastryvs/d2/_apis/work/boardcolumns?api-version=5.1 --user vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya  -o ouput.json' >test.txt
+ //influx()
+  echo " ++++++++++++ $response"
+
+if(response == "204" || response == "200")
+{
+ echo " Data pushed into influxDB "
 }
+else
+{
+ error("Error while pushing")
+}
+ }
