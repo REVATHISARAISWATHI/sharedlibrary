373d477634d1c960984870b5aaa09cdd6a74c21b
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-14 15:36:39 +0530

    Update Aboardcolumn.groovy

--- vars/Aboardcolumn.groovy
+++ vars/Aboardcolumn.groovy
@@ -13,7 +13,7 @@ sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:808
 def call()
 {
  
- sh  """curl  -s -XGET https://dev.azure.com/vickysastryvs/d2/_apis/work/boardcolumns?api-version=5.1 --user vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya >test.txt -o output.json  """
+ sh  """curl  -s -i -XGET https://dev.azure.com/vickysastryvs/d2/_apis/work/boardcolumns?api-version=5.1 --user vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya >test.txt -o output.json  """
  
  def res=new File('/var/lib/jenkins/workspace/' +JOB_NAME + '/test.txt').text 
   echo " ++++++++++++ $res"
