666affc12e74e6fddfbbe76e1f2c0b29630f65ee
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-14 12:28:53 +0530

    Update lastsuccessfulbuild.groovy

--- vars/lastsuccessfulbuild.groovy
+++ vars/lastsuccessfulbuild.groovy
@@ -1,3 +1,15 @@
+import groovy.json.*
+
+@NonCPS
+create(){
+def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+def lsbno = resultJson.number
+  echo "$lsb"
+sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'jenkins lastsuccessbuildno=${lsb}' 
+"""
+}
 def call(JSON){
 def jsonString = JSON
 def jsonObj = readJSON text: jsonString
