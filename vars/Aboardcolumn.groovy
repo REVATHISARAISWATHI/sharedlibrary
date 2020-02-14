7d35d180796df4524609819b713fbfe79ba9c209
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-14 15:13:48 +0530

    Update Aboardcolumn.groovy

--- vars/Aboardcolumn.groovy
+++ vars/Aboardcolumn.groovy
@@ -16,8 +16,8 @@ def call()
  sh  """curl -i -s -XGET https://dev.azure.com/vickysastryvs/d2/_apis/work/boardcolumns?api-version=5.1 --user vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya   >test.txt """
  
 //influx()
- new File('/var/lib/jenkins/workspace/' +JOB_NAME + '/test.txt').withReader { line = it.readLine() }  
-  echo " ++++++++++++ $line"
+ def res=new File('/var/lib/jenkins/workspace/' +JOB_NAME + '/test.txt').txt 
+  echo " ++++++++++++ $res"
 
 if(response == "200")
 {
