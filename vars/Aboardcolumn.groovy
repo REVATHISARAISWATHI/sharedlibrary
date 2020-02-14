393ad4c8b0088f45687d9241fe7a494e57638156
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-14 15:32:12 +0530

    Update Aboardcolumn.groovy

--- vars/Aboardcolumn.groovy
+++ vars/Aboardcolumn.groovy
@@ -13,9 +13,9 @@ sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:808
 def call()
 {
  
- sh  """curl -i -s -XGET https://dev.azure.com/vickysastryvs/d2/_apis/work/boardcolumns?api-version=5.1 --user vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya  -o output.json >test.txt """
+ sh  """curl  -s -XGET https://dev.azure.com/vickysastryvs/d2/_apis/work/boardcolumns?api-version=5.1 --user vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya  -o output.json  """
  
- def res=new File('/var/lib/jenkins/workspace/' +JOB_NAME + '/test.txt').text 
+ def res=new File('/var/lib/jenkins/workspace/' +JOB_NAME + '/output.json').text 
   echo " ++++++++++++ $res"
 
 if(res.contains("200"))
