0e55888172bfaab771ec24f791ead04347f73718
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-14 14:44:38 +0530

    Update lastfailurebuild.groovy

--- vars/lastfailurebuild.groovy
+++ vars/lastfailurebuild.groovy
@@ -5,9 +5,9 @@ create(){
 def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
-def lfb = resultJson.number
-  echo "$lfb"
-sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'jenkins lastfailurebuildno=${lfb}' 
+def lfbno = resultJson.number
+  echo "$lfbno"
+sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'jenkins lastfailurebuildno=${lfbno}' 
 """
 }
 def call(JSON){
@@ -17,4 +17,5 @@ def jsonObj = readJSON text: jsonString
 String a=jsonObj.ci1.jobs.job.job_name
 String jname=a.replaceAll("\\[", "").replaceAll("\\]","");
   sh "curl -s http://http://18.219.219.69:8080/job/${jname}/lastFailedBuild/api/json --user vj:11e428e94b267ffbab27fa713e2da8e6e8 -o output.json"
+create()
 }
