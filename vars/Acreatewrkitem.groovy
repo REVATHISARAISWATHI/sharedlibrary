3c6543ddb0dbcf1b98a58eaaa25ec9a3f1303b88
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-20 23:02:27 +0530

    Update Acreatewrkitem.groovy

--- vars/Acreatewrkitem.groovy
+++ vars/Acreatewrkitem.groovy
@@ -1,4 +1,4 @@
 def call()
 {
-sh "curl -X POST -v -i -H  "Accept: application/json" -H "Content-Type: application/json-patch+json" -d "@repo.json" -u vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya "https://dev.azure.com/vickysastryvs/d2/_apis/wit/workitems/%24Epic?api-version=5.1" "  
+sh 'curl -X POST -v -i -H  "Accept: application/json" -H "Content-Type: application/json-patch+json" -d "@repo.json" -u vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya "https://dev.azure.com/vickysastryvs/d2/_apis/wit/workitems/%24Epic?api-version=5.1" '
 }
