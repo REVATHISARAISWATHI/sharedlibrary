06dd4ddf75994addef25da914793d5a389a75221
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-21 15:57:13 +0530

    Update Aaddcomment.groovy

--- vars/Aaddcomment.groovy
+++ vars/Aaddcomment.groovy
@@ -1,5 +1,4 @@
 def call(organization,projectname,id)
 {
-sh "curl -X POST  -H  Accept: application/json -H  'Content-Type: application/json'  -u vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya https://dev.azure.com/${organization}/${projectname}/_apis/wit/workItems/${id}/comments?api-version=5.1-preview.3 \
-   -d { "text": "Moving to the right area path" } "
+sh "curl -X POST  -H  Accept: application/json -H  'Content-Type: application/json' \ -d { "text": "Moving to the right area path" }  \  -u vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya https://dev.azure.com/${organization}/${projectname}/_apis/wit/workItems/${id}/comments?api-version=5.1-preview.3 "
 }
