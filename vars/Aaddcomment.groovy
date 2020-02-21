4f4623d9de3d3dfcc17f1ff06a80f99087867b87
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-21 15:44:27 +0530

    Update Aaddcomment.groovy

--- vars/Aaddcomment.groovy
+++ vars/Aaddcomment.groovy
@@ -1,3 +1,4 @@
 def call(organization,projectname,id)
 {
-sh "curl -X POST  -H  Accept: application/json -H  'Content-Type: application/json' -d @command.json -u vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya https://dev.azure.com/${organization}/${projectname}_apis/wit/workItems/${id}/comments?api-version=5.1-preview.3 "}
+sh "curl -X POST  -H  Accept: application/json -H  'Content-Type: application/json' -d @command.json -u vickysastry.vs@outlook.com:zsxapkj3zwk6rtz7zm4tyli7ayk7yt5yehp5ic7erlec4xsf7tya https://dev.azure.com/${organization}/${projectname}/_apis/wit/workItems/${id}/comments?api-version=5.1-preview.3 "
+}
