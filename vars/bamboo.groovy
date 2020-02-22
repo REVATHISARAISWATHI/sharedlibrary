2a64deb80285cb0b5d6c822f2746f7a0593ac051
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-22 16:19:57 +0530

    Update bamboo.groovy

--- vars/bamboo.groovy
+++ vars/bamboo.groovy
@@ -1,4 +1,4 @@
 def call(IP)
 {
-  sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps '${IP}'/rest/api/latest/result/LAT-WEB-143.json?buildstate "
+  sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps ${IP}/rest/api/latest/result/LAT-WEB-143.json?buildstate "
 }
