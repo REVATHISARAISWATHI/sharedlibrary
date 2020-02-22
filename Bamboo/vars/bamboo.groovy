b0f240506e61859d23e94647d4df273ad49bd316
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-22 15:30:06 +0530

    Update bamboo.groovy

--- Bamboo/vars/bamboo.groovy
+++ Bamboo/vars/bamboo.groovy
@@ -1,5 +1,5 @@
 def call()
 {
 sh "curl -X GET -H  -u rig:rigaDapt@devOps http://18.220.143.53:8085/rest/api/latest/result/LAT-WEB.json?max-result=1 "
-
+  println(${ManualBuildTriggerReason.getUserName()})
 }
