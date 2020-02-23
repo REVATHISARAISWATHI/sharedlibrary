2b4df13fdd79750f82127afb241bf9081e9a53de
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-23 17:08:08 +0530

    Update bamboonobuild.groovy

--- vars/bamboonobuild.groovy
+++ vars/bamboonobuild.groovy
@@ -3,7 +3,7 @@ def call(IP)
 {
   //sh "curl -X GET -s -u rig:rigaDapt@devOps ${IP}/rest/api/latest/chart.json?reportKey=com.atlassian.bamboo.plugin.system.reports%3AnumberOfBuilds%26buildKeys=LAT-WEB%26groupByPeriod=YEAR%26dateFilter=RANGE%26dateFrom=22%2F2%2F2020%26dateTo=23%2F2%2F2020  -o  ouput.json"
  sh """ curl -X GET \
-  '${IP}/rest/api/latest/chart.json?reportKey=com.atlassian.bamboo.plugin.system.reports%3AnumberOfBuilds&buildKeys=LAT-WEB&groupByPeriod=YEAR&dateFilter=RANGE&dateFrom=23%2F2%2F2020&dateTo=24%2F2%2F2020' \
+  '${IP}/rest/api/latest/chart.json?reportKey=com.atlassian.bamboo.plugin.system.reports%3AnumberOfBuilds&buildKeys=LAT-WEB&groupByPeriod=YEAR&dateFilter=RANGE&dateFrom=22%2F2%2F2020&dateTo=23%2F2%2F2020' \
   -H 'authorization: Basic cmlnOnJpZ2FEYXB0QGRldk9wcw==' \
   -H 'cache-control: no-cache' \
   -H 'postman-token: ad68a811-ecd6-864c-2685-5f8c15a81cc3'  -o ouput.json
@@ -14,5 +14,15 @@ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/
 def resultJson = jsonSlurper.parse(reader)
 def nobuild=resultJson.imageMap
 println(nobuild)
- //def val = user.split('')
+  if(nobuild!="null")
+  {
+    def val = nobuild.split('title=\"')
+    println(val[0])
+  }
+  else
+  {
+    def count=0
+    println("the no of build is" count)
+  }
+ 
 }
