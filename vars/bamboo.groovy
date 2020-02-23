fdacef3d9a07a7c503b289f71705802604bd71b8
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-23 11:39:27 +0530

    Update bamboo.groovy

--- vars/bamboo.groovy
+++ vars/bamboo.groovy
@@ -1,4 +1,15 @@
+import groovy.json.*
 def call(IP)
 {
-  sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps ${IP}/rest/api/latest/result/LAT-WEB-143.json?buildstate "
+  sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps ${IP}/rest/api/latest/result/LAT-WEB-143.json?buildstate -o ouput.json"
+ def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+def state=resultJson.state
+  println(state)
+  if(state.equals("Successful"))
+  {
+    def user=resultJson.buildReason
+    println(user)
+  }
 }
