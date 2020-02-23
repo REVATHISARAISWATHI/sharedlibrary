18c777e4bfcbceac5329dd3abfd4be763a6fe9d2
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-23 16:33:15 +0530

    Update bamboonobuild.groovy

--- vars/bamboonobuild.groovy
+++ vars/bamboonobuild.groovy
@@ -1,7 +1,9 @@
 import groovy.json.*
 def call(IP)
 {
-  sh "curl -X GET -s -u rig:rigaDapt@devOps ${IP}/rest/api/latest/chart.json?reportKey=com.atlassian.bamboo.plugin.system.reports:numberOfBuilds&buildKeys=LAT-WEB&groupByPeriod=YEAR&dateFilter=RANGE&dateFrom=22/2/2020&dateTo=23/2/2020  -o  ouput.json"
+  sh "curl -X GET -s -u rig:rigaDapt@devOps ${IP}/rest/api/latest/chart.json?reportKey%3Dcom.atlassian.bamboo.plugin.system.reports%3AnumberOfBuilds&buildKeys%3DLAT-WEB&groupByPeriod%3DYEAR&dateFilter%3DRANGE&dateFrom%3D22/2/2020&dateTo%3D23/2/2020  -o  ouput.json"
+  
+ 
  def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
