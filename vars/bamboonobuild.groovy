e9e56a9562fc6eee1166a3f8f12bba2723690b1f
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-23 18:13:40 +0530

    Update bamboonobuild.groovy

--- vars/bamboonobuild.groovy
+++ vars/bamboonobuild.groovy
@@ -1,4 +1,24 @@
 import groovy.json.*
+  @NonCPS
+success()
+{
+   sh """curl -X GET \
+  'http://18.220.143.53:8085/rest/api/latest/chart.json?reportKey=com.atlassian.bamboo.plugin.system.reports%3AnumberOfFailures&buildKeys=LAT-WEB&groupByPeriod=YEAR&dateFilter=RANGE&dateFrom=22%2F2%2F2020&dateTo=23%2F2%2F2020' \
+  -H 'authorization: Basic cmlnOnJpZ2FEYXB0QGRldk9wcw==' \
+  -H 'cache-control: no-cache' \
+  -H 'postman-token: 54f9ea34-9831-be64-6e71-45f1e893f2eb'  -o ouput.json
+  """
+    def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+    def nbuild=resultJson.imageMap
+      def val2 = nobuild.split('title=\"')
+     def builds = val2[1].split(' ')
+    def Failbuild=builds[3]
+    println(Failbuild)
+    def successbuild=totalbuild-Failbuild
+     println(successbuild)
+}
 def call(IP)
 {
   //sh "curl -X GET -s -u rig:rigaDapt@devOps ${IP}/rest/api/latest/chart.json?reportKey=com.atlassian.bamboo.plugin.system.reports%3AnumberOfBuilds%26buildKeys=LAT-WEB%26groupByPeriod=YEAR%26dateFilter=RANGE%26dateFrom=22%2F2%2F2020%26dateTo=23%2F2%2F2020  -o  ouput.json"
@@ -23,22 +43,7 @@ println(nobuild)
      def vals = val[1].split(' ')
     def totalbuild=vals[3]
     println(totalbuild)
-    sh """curl -X GET \
-  'http://18.220.143.53:8085/rest/api/latest/chart.json?reportKey=com.atlassian.bamboo.plugin.system.reports%3AnumberOfFailures&buildKeys=LAT-WEB&groupByPeriod=YEAR&dateFilter=RANGE&dateFrom=22%2F2%2F2020&dateTo=23%2F2%2F2020' \
-  -H 'authorization: Basic cmlnOnJpZ2FEYXB0QGRldk9wcw==' \
-  -H 'cache-control: no-cache' \
-  -H 'postman-token: 54f9ea34-9831-be64-6e71-45f1e893f2eb'  -o ouput.json
-  """
-    def jsonSlurper1 = new JsonSlurper()
-def reader1 = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
-def resultJson1 = jsonSlurper1.parse(reader1)
-    def nbuild=resultJson1.imageMap
-      def val2 = nobuild.split('title=\"')
-     def builds = val2[1].split(' ')
-    def Failbuild=builds[3]
-    println(Failbuild)
-    def successbuild=totalbuild-Failbuild
-     println(successbuild)
+   success()
   }
   else
   {
