6de398d865aacc8ec2738f81ba5c3dc03e06201a
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-24 17:23:56 +0530

    Update array_gitlab.groovy

--- vars/array_gitlab.groovy
+++ vars/array_gitlab.groovy
@@ -8,11 +8,18 @@ def resultJson = jsonSlurper.parse(reader)
   def size = resultJson.size()
   println(size)
   for(i=0;i<size;i++){
-def uname=resultJson[i].committer_name
-  println(uname)
+def list[i]=resultJson[i].committer_name
+  println(list[i])
 def commit=resultJson[i].committed_date
 println(commit)
   }
+  for(i=0;i<size;i++){
+def uname=resultJson[i].committer_name
+    if(uname==list[i]){ count++ }
+    else { count1++ }
+    println(count)
+    println(count1)
+ 
   //ef val=arr  { reg
   //println(val)
 }
