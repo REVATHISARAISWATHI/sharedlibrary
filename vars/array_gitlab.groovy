e293b85a38ea67f97f96c7b90ba5ff08a89d9daf
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-24 16:14:16 +0530

    Update array_gitlab.groovy

--- vars/array_gitlab.groovy
+++ vars/array_gitlab.groovy
@@ -1,7 +1,7 @@
 import groovy.json.*
 def call(IP)
 {
-  sh "curl -X GET -s -u Priyakumar:pri4jay16 https://gitlab.com/api/v4/projects/17097955/repository/contributors -o ouput.json"
+  sh "curl -X GET -s -u Priyakumar:pri4jay16 https://gitlab.com/api/v4/projects/17097955/repository/commits -o ouput.json"
   def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
