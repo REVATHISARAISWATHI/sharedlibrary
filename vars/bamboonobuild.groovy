1491e7dae337d70b222cbb84b259543f30d00e43
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 07:32:29 +0530

    Update bamboonobuild.groovy

--- vars/bamboonobuild.groovy
+++ vars/bamboonobuild.groovy
@@ -58,7 +58,7 @@ def resultJson = jsonSlurper.parse(reader)
     def Failbuild=builds[2]
     println(Failbuild)
   println(totalbuilds)
-  def var=${totalbuilds}
+  def var="${totalbuilds}"
   def successbuild=var-Failbuild
      println(successbuild)
 }
