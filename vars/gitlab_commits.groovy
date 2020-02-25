451c370987424c67b2cca55cfdb1f2c27425e2ad
vijayvj3 <55652667+vijayvj3@users.noreply.github.com>
2020-02-25 11:25:02 +0530

    Create gitlab_commits.groovy

---
+++ vars/gitlab_commits.groovy
@@ -0,0 +1,10 @@
+import groovy.json.*
+def call()
+{
+  sh "curl -X GET -s -u Priyakumar:pri4jay16 https://gitlab.com/api/v4/projects/17097955/repository/commits -o ouput.json"
+  def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+  def size = resultJson.size()
+  println(size)
+  }
