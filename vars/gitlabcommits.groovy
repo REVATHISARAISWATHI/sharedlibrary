ccbb72a9ab8a8061121796c8daf066e174ec9e1b
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-25 11:56:21 +0530

    Update gitlabcommits.groovy

--- vars/gitlabcommits.groovy
+++ vars/gitlabcommits.groovy
@@ -9,7 +9,7 @@ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/
 def resultJson = jsonSlurper.parse(reader)
 def projId = resultJson.id*/
  
-sh "curl -X GET  -H -d  -u Priyakumar:pri4jay16 https://gitlab.com/api/v4/projects/$projId/repository/commits -o output.json"
+sh "curl -X GET  -H -d  -u Priyakumar:pri4jay16 https://gitlab.com/api/v4/projects/17097955/repository/commits -o output.json"
 
 
 }
