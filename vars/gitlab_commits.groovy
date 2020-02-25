9147f0c0c4cc9de7f689c5bbb23d9333b1aab568
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-25 16:22:08 +0530

    Update gitlab_commits.groovy

--- vars/gitlab_commits.groovy
+++ vars/gitlab_commits.groovy
@@ -1,32 +1,55 @@
 import groovy.json.*
-def call()
-{
-  sh "curl -X GET -s -u Priyakumar:pri4jay16 https://gitlab.com/api/v4/projects/17097955/repository/commits -o ouput.json"
-  def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
-def resultJson = jsonSlurper.parse(reader)
-  def size = resultJson.size()
-  println(size)
-  for(i=0;i<size;i++){
-    //def commit=resultJson[i].author_email
-println(resultJson[i])
-   /*commit[i]=resultJson[i].author_email
-    println(commit[i])*/
-     List<String> commit=new ArrayList<String>();
-   for(int j=0;j<size;j++)
-    {
-      /*count[j]=0
-   for(int i=0;i<size;i++){
-      //if((resultJson.commit.author.name[i]).equals( collab[j] ))
-         if((resultJson.commit.author.name[i]).equals( collab[j] ))
-     count[j]++
-       println(count)*/
-      if(author_email[j].equals(priyamk061998@gmail.com))
-      commit.add(JsonOutput.toJson(resultJson.commit.author_email[j]))
-         }
-         } 
-      
-    
-    
+
+def call(jsondata){
+def jsonString = jsondata
+def jsonObj = readJSON text: jsonString
+
+int ecount = jsonObj.config.emails.email.size()
+println("No of users "+ ecount)
+ 
+// withCredentials([usernamePassword(credentialsId: 'bitbucket_cred', passwordVariable: 'pass', usernameVariable: 'userId')]) {
+sh "curl -X GET  -H -d  -u Priyakumar:pri4jay16 https://gitlab.com/api/v4/projects/17097955/repository/commits -o output.json" 
+ } 
+def jsonSlurper = new JsonSlurper()
+def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"))
+def total = resultJson.size
+ echo "Total no.of commits in ${repoName} $total"
+def commiter=1
+List<String> JSON = new ArrayList<String>();
+
+for(i=0;i<ecount;i++)
+ {
+  for(j=0;j<total;j++)
+  {
+   if(jsonObj.config.emails.email[i]==resultJson.values.author[j].emailAddress)
+   {
+	   JSON.add(JsonOutput.toJson(resultJson.values[j]))
+  //  y = resultJson.values[j];
+    //echo "y >>commiter'${commiter}'.txt"
+	//   sh "echo '${y}' >>commiter'${commiter}'.json "
+    }
+	  //Long commitdate=resultJson.values.committerTimestamp[j]
+	  //def name=resultJson.values.author[j].name
+	  //println(name)
+	  //def email=resultJson.values.author[j].emailAddress
+	  //println(email)
+	 // sh "echo contributorsName :'${name}', contributorsEmail :'${email}' >>commiter'${commiter}'.txt "
+         
+	
+   }
+	
   }
-}
+ 
+/*
+def jsonSlurper = new JsonSlurper()
+def jsonString1 = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"))	
+//def jsonString1 = output.json
+def jsonObj1 = readJSON text: jsonString1	
+String total=jsonObj1.size
+String commits=total.replaceAll("\\[", "").replaceAll("\\]","");
+println(commits)
+	
+*/	
+println(JSON)
+
+ }
