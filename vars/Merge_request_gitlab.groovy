1843cc61fe1c1b31f132cea7ee280320cf8c0403
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-25 18:13:40 +0530

    Update Merge_request_gitlab.groovy

--- vars/Merge_request_gitlab.groovy
+++ vars/Merge_request_gitlab.groovy
@@ -1,6 +1,9 @@
   def call(){
-
-
    // withCredentials([usernamePassword(credentialsId: 'nexus_cred', passwordVariable: 'password', usernameVariable:'username')]) {
 sh "curl -X GET -i -H  -d  -u Priyakumar:pri4jay16 https://gitlab.com/api/v4/projects/17097955/merge_requests -o output.json"
+   def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+def total = resultJson.size()
+   println(total)
 }
