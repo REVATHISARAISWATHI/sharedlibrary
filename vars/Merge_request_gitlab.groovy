6d7a33d48ef10c1c8c11fdbb2aaebd5d5ae9c1d8
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-25 18:04:36 +0530

    Update Merge_request_gitlab.groovy

--- vars/Merge_request_gitlab.groovy
+++ vars/Merge_request_gitlab.groovy
@@ -2,5 +2,5 @@
 
 
    // withCredentials([usernamePassword(credentialsId: 'nexus_cred', passwordVariable: 'password', usernameVariable:'username')]) {
-sh "curl -X GET -i -H  -d  -u priyakumar:pri4jay16 https://gitlab.com/api/v4/projects/17097955/merge_requests"
+sh "curl -X GET -i -H  -d  -u Priyakumar:pri4jay16 https://gitlab.com/api/v4/projects/17097955/merge_requests -o output.json"
 }
