987d4420207695dbddbe0d6e026400b7a8bd1ae5
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-25 18:15:09 +0530

    Update Merge_request_gitlab.groovy

--- vars/Merge_request_gitlab.groovy
+++ vars/Merge_request_gitlab.groovy
@@ -1,4 +1,6 @@
-  def call(){
+import groovy.json.*
+
+def call(){
    // withCredentials([usernamePassword(credentialsId: 'nexus_cred', passwordVariable: 'password', usernameVariable:'username')]) {
 sh "curl -X GET -i -H  -d  -u Priyakumar:pri4jay16 https://gitlab.com/api/v4/projects/17097955/merge_requests -o output.json"
    def jsonSlurper = new JsonSlurper()
