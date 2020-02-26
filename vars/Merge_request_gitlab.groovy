34605b9a48c344b1be106679035c60bb29c583e2
vijayvj3 <55652667+vijayvj3@users.noreply.github.com>
2020-02-26 13:16:09 +0530

    Update Merge_request_gitlab.groovy

--- vars/Merge_request_gitlab.groovy
+++ vars/Merge_request_gitlab.groovy
@@ -1,15 +1,16 @@
 import groovy.json.*
    int ids;
-   def call(jsondata){
+   def gitlab(jsondata){
       def jsonString = jsondata
       def jsonObj = readJSON text: jsonString
      withCredentials([usernamePassword(credentialsId: 'gitlab_cred', passwordVariable: 'password', usernameVariable:'username')]) {
       sh "curl -X GET -i -H  -d  -u $username:$password https://gitlab.com/api/v4/users/5418155/projects -o output.json"
      }
-       def jsonSlurper = new JsonSlurper()
-   def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
+   def jsonSlurper = new JsonSlurper()
+ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
 def usertotal = resultJson.size()
+      println(usertotal)
       for(i=0;i<usertotal;i++)
          {
             if(jsonObj.scm.projects.project.project_name==resultJson[i].name)
