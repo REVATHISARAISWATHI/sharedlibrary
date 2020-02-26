9d8ce74d3fa2bac794eec165a0c3fb4abe8f54d1
vijayvj3 <55652667+vijayvj3@users.noreply.github.com>
2020-02-26 15:26:39 +0530

    Update Merge_request_gitlab.groovy

--- vars/Merge_request_gitlab.groovy
+++ vars/Merge_request_gitlab.groovy
@@ -1,24 +1,19 @@
 import groovy.json.*
-   int ids;
+   long ids;
    def call(jsondata){
       def jsonString = jsondata
       def jsonObj = readJSON text: jsonString
-      String a=jsonObj.scm.projects.project.project_name
-    String Name=a.replaceAll("\\[", "").replaceAll("\\]","");
      withCredentials([usernamePassword(credentialsId: 'gitlab_cred', passwordVariable: 'password', usernameVariable:'username')]) {
-      sh "curl -X GET -u $username:$password  https://gitlab.com/api/v4/users/5418155/projects -o output.json"
+      sh "curl -X GET    -u $username:$password https://gitlab.com/api/v4/users/5418155/projects -o output.json"
      }
    def jsonSlurper = new JsonSlurper()
- def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"),"UTF-8"))
+ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
 def usertotal = resultJson.size()
       println(usertotal)
-      println(Name)
-      println(resultJson[0].name)
-      
-      for(i=0;i<=usertotal;i++)
+      for(i=0;i<usertotal;i++)
          {
-            if(Name==resultJson[i].name)
+            if(jsonObj.scm.projects.project.project_name==resultJson[i].name)
             {
                def id1 = resultJson[i].id
                println(id1)
