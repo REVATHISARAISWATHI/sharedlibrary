5faa7e8bdf810e0dea4442883544e8241743720b
vijayvj3 <55652667+vijayvj3@users.noreply.github.com>
2020-02-26 13:00:45 +0530

    Update Merge_request_gitlab.groovy

--- vars/Merge_request_gitlab.groovy
+++ vars/Merge_request_gitlab.groovy
@@ -21,6 +21,7 @@ def usertotal = resultJson.size()
    def commit(id1){
       withCredentials([usernamePassword(credentialsId: 'gitlab_cred', passwordVariable: 'password', usernameVariable:'username')]) {
          sh "curl -X GET -i -H  -d  -u $username:$password https://gitlab.com/api/v4/projects/${id1}/merge_requests -o output.json"
+      }
    def jsonSlurper = new JsonSlurper()
    def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
