052777cf7897d1696a6ba310e4a24258b462bb46
vijayvj3 <55652667+vijayvj3@users.noreply.github.com>
2020-02-26 18:31:41 +0530

    Update Merge_request_gitlab.groovy

--- vars/Merge_request_gitlab.groovy
+++ vars/Merge_request_gitlab.groovy
@@ -21,14 +21,21 @@ def usertotal = resultJson.size()
             if(Name==resultJson[i].name)
             {
                def id1 = resultJson[i].id 
-               println(id1.toInteger())
-             return id1
+               //println(id1.toInteger())
+             //return id1
             }
          }
+       withCredentials([usernamePassword(credentialsId: 'gitlab_cred', passwordVariable: 'password', usernameVariable:'username')]) {
+         sh "curl -X GET -i -H  -d  -u $username:$password https://gitlab.com/api/v4/projects/${id1}/merge_requests -o ouput.json"
+      }
+       def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
+def resultJson1 = jsonSlurper.parse(reader)
+      def total = resultJson.size()
+   println(total)
    }
 
 
-   def commit(ids1){
+ /*  def commit(ids1){
       println(ids1)
       withCredentials([usernamePassword(credentialsId: 'gitlab_cred', passwordVariable: 'password', usernameVariable:'username')]) {
          sh "curl -X GET -i -H  -d  -u $username:$password https://gitlab.com/api/v4/projects/${ids1}/merge_requests -o output.json"
@@ -39,3 +46,4 @@ def resultJson = jsonSlurper.parse(reader)
 def total = resultJson.size()
    println(total)
 }
+*/
