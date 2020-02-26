0b0fe905a5f1b1c8738842132f218a331c24f2d8
vijayvj3 <55652667+vijayvj3@users.noreply.github.com>
2020-02-26 12:55:30 +0530

    Update Merge_request_gitlab.groovy

--- vars/Merge_request_gitlab.groovy
+++ vars/Merge_request_gitlab.groovy
@@ -1,10 +1,28 @@
 import groovy.json.*
-
-def call(){
-   // withCredentials([usernamePassword(credentialsId: 'nexus_cred', passwordVariable: 'password', usernameVariable:'username')]) {
-sh "curl -X GET -i -H  -d  -u Priyakumar:pri4jay16 https://gitlab.com/api/v4/projects/17097955/merge_requests -o output.json"
+   def gitlab(jsondata){
+      def jsonString = jsondata
+      def jsonObj = readJSON text: jsonString
+     withCredentials([usernamePassword(credentialsId: 'gitlab_cred', passwordVariable: 'password', usernameVariable:'username')]) {
+      sh "curl -X GET -i -H  -d  -u $username:$password https://gitlab.com/api/v4/users/5418155/projects -o output.json"
+     }
+       def jsonSlurper = new JsonSlurper()
+   def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+def usertotal = resultJson.size()
+      for(i=0;i<usertotal;i++)
+         {
+            if(jsonObj.scm.projects.project.project_name==resultJson[i].name)
+            {
+               def id1 = resultJson[i].id
+.               return id1
+            }
+         }
+   }
+   def call(id1){
+      withCredentials([usernamePassword(credentialsId: 'gitlab_cred', passwordVariable: 'password', usernameVariable:'username')]) {
+         sh "curl -X GET -i -H  -d  -u $username:$password https://gitlab.com/api/v4/projects/${id1}/merge_requests -o output.json"
    def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
+   def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
 def total = resultJson.size()
    println(total)
