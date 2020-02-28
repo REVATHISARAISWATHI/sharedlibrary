b43328b4dd01a2fa64786e6eb9d094acd0571ab0
younu8084 <younu8084@gmail.com>
2020-02-28 15:35:24 +0530

    Update bamboonew.groovy

--- vars/bamboonew.groovy
+++ vars/bamboonew.groovy
@@ -79,7 +79,7 @@ println(mailcount)
     //def u =new user()
    //def p = new person();
     //people.add(new Person(JsonOutput.toJson(resultJson.results.result[i])));
-    USER[k]=resultJson.results.result[i]
+    USER[k].add(resultJson.results.result[i])
    // USER.collect new person(name:JsonOutput.toJson(resultJson.results.result[i]) }
     //p.setemail(email)
  // u.setdata(JsonOutput.toJson(resultJson.results.result[i]))
