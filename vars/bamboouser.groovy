d8bc2c39daba7c1ac982d2a2a497cf67f5f3a31a
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-02 16:32:57 +0530

    Update bamboouser.groovy

--- vars/bamboouser.groovy
+++ vars/bamboouser.groovy
@@ -68,10 +68,10 @@ println(mailcount)
    }
    }
    cns=USERS.size()
-   LISTSUCCESS.add(["email":email,"success":JsonOutput.toJson(USERS),"Success_cnt":cns])
+   LISTSUCCESS.add(JsonOutput.toJson(["email":email,"success":USERS,"Success_cnt":cns]))
    USERS.clear()
    cnf=USERF.size()
-   LISTFAILURE.add(["email":email,"failure":JsonOutput.toJson(USERF),"Success_cnt":cnf])
+   LISTFAILURE.add(JsonOutput.toJson(["email":email,"failure":USERF,"Success_cnt":cnf]))
    USERF.clear()
    }
 	for(i=0;i<50;i++)
