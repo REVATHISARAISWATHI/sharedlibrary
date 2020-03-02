b4600af74e822acbf89d1fc6f380cd401f65f443
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-02 11:30:02 +0530

    Update bamboo.groovy

--- vars/bamboo.groovy
+++ vars/bamboo.groovy
@@ -73,7 +73,7 @@ file.write(json_beauty)*/
  println(cnf)
  println(FAILURE)
  def jsonArray
-jsonArray.push($(success).val)
+jsonArray.push($(SUCCESS).val)
 jsonArray.push($(FAILURE).val)
 var my = JSON.stringify(jsonArray)
  //def json = {"success" : SUCCESS,"successbuild_cnt" : SUCCESS.size(),"failure" : FAILURE,"failurebuild_cnt" : FAILURE.size()}
