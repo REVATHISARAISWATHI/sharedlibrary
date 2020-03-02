e1e6b6cc6c93b52a85d03085c9966b302213b152
younu8084 <younu8084@gmail.com>
2020-03-02 19:06:34 +0530

    Update bamboouser.groovy

--- vars/bamboouser.groovy
+++ vars/bamboouser.groovy
@@ -105,7 +105,7 @@ def jsonObj1 = readJSON text: json
 
 	println(jsonObj1)*/
 
-jsonbuilder=jsonBuilder.replaceAll("\","");
+jsonBuilder=jsonBuilder.replaceAll("\","");
 				   println(jsonBuilder)
 	
 
