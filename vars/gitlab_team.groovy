6302832a2cc70b302d7a9d611dc2ebf1d7670942
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-03-04 16:43:00 +0530

    Update gitlab_team.groovy

--- vars/gitlab_team.groovy
+++ vars/gitlab_team.groovy
@@ -1,6 +1,12 @@
-def call(gitlab){
-
-  println(gitlab)
-
-
+import groovy.json.*
+import groovy.json.JsonOutput
+def call(gitlab)
+{
+def jsonString = bamboo1
+def jsonObj = readJSON text: jsonString
+  println(jsonObj)
+  def cnt =jsonObj.Bamboo.teamsuccessbuild_cnt
+ // def res=bamboo1.bamboo.teamsuccessbuild_cnt
+ // def obj = JSON.parse(bamboo1)
+ println(cnt)
 }
