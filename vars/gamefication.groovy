6341ee3252e0dc0865a19fd4305acf4430923796
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-04 16:21:09 +0530

    Update gamefication.groovy

--- vars/gamefication.groovy
+++ vars/gamefication.groovy
@@ -5,9 +5,9 @@ def call(bamboo1)
 //println(bamboo1)
   def jsonSlurper = new JsonSlurper()
   //def reader = new BufferedReader(bamboo1)
- // def resultJson = jsonSlurper.parse(bamboo1)
-  //def cnt =resultJson.bamboo.teamsuccessbuild_cnt
+  String resultJson = jsonSlurper.parse(bamboo1)
+  def cnt =resultJson.bamboo.teamsuccessbuild_cnt
  // def res=bamboo1.bamboo.teamsuccessbuild_cnt
-  def obj = JSON.parse(bamboo1)
- println(obj)
+ // def obj = JSON.parse(bamboo1)
+ println(cnt)
 }
