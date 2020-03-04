9457737dbfb9bd4ce59d7914780ce670e05763e8
younu8084 <younu8084@gmail.com>
2020-03-04 16:38:04 +0530

    Update gamefication.groovy

--- vars/gamefication.groovy
+++ vars/gamefication.groovy
@@ -5,7 +5,7 @@ def call(bamboo1)
 def jsonString = bamboo1
 def jsonObj = readJSON text: jsonString
   println(jsonObj)
-  def cnt =jsonObj.bamboo.teamsuccessbuild_cnt
+  def cnt =jsonObj.Bamboo.teamsuccessbuild_cnt
  // def res=bamboo1.bamboo.teamsuccessbuild_cnt
  // def obj = JSON.parse(bamboo1)
  println(cnt)
