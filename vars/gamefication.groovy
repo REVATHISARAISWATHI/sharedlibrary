9a3e9bfe97b31bd93f92d631bdd19ae0703bba10
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-05 17:15:14 +0530

    Update gamefication.groovy

--- vars/gamefication.groovy
+++ vars/gamefication.groovy
@@ -1,23 +1,50 @@
 import groovy.json.*
 import groovy.json.JsonOutput
-def call(bamboo1)
+def call(jsondata,bamboo1)
 {
-def jsonString = bamboo1
+ef jsonString = jsondata
 def jsonObj = readJSON text: jsonString
+int ecount = jsonObj.config.emails.email.size()
+
+ 
+ List<String> JSON = new ArrayList<String>();
+  List<String> LIST = new ArrayList<String>();
+  List<String> JSON1 = new ArrayList<String>();
+
+  for(j=0;j<ecount;j++)
+   {
+	 def email=jsonObj.config.emails.email[j] 
+	   
+	    
+     // name="Bamboo"
+    //  def jsonStringb = bamboo
+def jsonObjb = readJSON text: bamboo1
   //println(jsonObj)
-  def scnt =jsonObj.Bamboo.teamsuccessbuild_cnt
-  def fcnt=jsonObj.Bamboo.teamfailurebuild_cnt
+  def scnt =jsonObjb.Bamboo.individualsuccess[j].Success_cnt
+  def fcnt =jsonObjb.Bamboo.individualfailure[j].Failure_cnt
+ def email1=jsonObjb.Bamboo.individualsuccess[j].email
+      
  // def res=bamboo1.bamboo.teamsuccessbuild_cnt
  // def obj = JSON.parse(bamboo1)
- //println(fcnt)
- def score=0
- if(scnt>10)
+ println(scnt)
+ 
+ if(email==email1 && scnt>10)
   {
    score=score+10 
+    LIST.add(["metric":"No of more successful builds","score":score])
+    score=0
   }
-   if(fcnt<2)
+     if(email==email1 && fcnt<2)
   {
- score=score+10
+   score=score+10 
+    LIST.add(["metric":"No of least failure builds","score":score])
+    score=0
   }
-  println(score)
+    }
+     JSON1=LIST.clone()
+   JSON.add(["email":email,"metrics":JSON1])
+    LIST.clear()
+    
+    
+  println(JSON)
 }
