a00c1c73e0428239e484c56cc03575fc790f98c3
younu8084 <younu8084@gmail.com>
2020-03-06 16:41:08 +0530

    Update game.groovy

--- vars/game.groovy
+++ vars/game.groovy
@@ -107,7 +107,7 @@ int total=jsonObjb.bitbucket.Individual_commits[j].Commit_count
 	def me=0
 	for(i=0;i<JSON.size();i++)
 	{
-for(j=1;j<=JSON.size();j++)
+for(j=1;j<JSON.size();j++)
 		{
 			
 			
